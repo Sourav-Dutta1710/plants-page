e93816854dc4084293aeea3a754fdc4b3527bcae
sourav-itobuz <sourav.dutta@itobuz.com>
2023-07-14 14:44:48 +0530

    third commit

--- js/main.js
+++ js/main.js
@@ -1,77 +1,76 @@
-let flag=-1;
-const images = ["black","beaker","white","black","beaker"];
-const labels = ["Rubber plants","String of pearls","Birds of paradise","Rubber plants","String of pearls"];
-const clients = ["second-model","third-model","model","second-model","third-model"];
+const images = ["black", "beaker", "white", "black", "beaker"];
+const labels = ["Rubber plants", "String of pearls", "Birds of paradise", "Rubber plants", "String of pearls"];
+const clients = ["second-model", "third-model", "model", "second-model", "third-model"];
+let flag = -1;
 
-function slide(slideRight,section){
-    document.getElementById("collectionItemLeft").classList.add("collectionItem");
-    document.getElementById("collectionItemMiddle").classList.add("collectionItem");
-    document.getElementById("collectionItemRight").classList.add("collectionItem");
-    document.getElementById("clientsItem").classList.add("collectionItem");
-    if(slideRight)
-      flag++;
-    else
-      flag--;
+function slide(slideRight, section) {
+  document.getElementById("collection-item-left").classList.add("collection-item");
+  document.getElementById("collection-item-middle").classList.add("collection-item");
+  document.getElementById("collection-item-right").classList.add("collection-item");
+  document.getElementById("clients-item").classList.add("collection-item");
 
-    if(slideRight && flag === 3)
+  if (slideRight && flag === 2)
     flag = 0;
+  else if (slideRight)
+    flag++;
+  else if (flag === -1)
+    flag = 1;
+  else if (flag === 0)
+    flag = 2;
+  else
+     flag--;
 
-    if(!slideRight && flag === -2)
-      flag = 1;
-    else if(!slideRight && flag === -1)
-      flag = 2;
+  if (section === 0)
+    showCollection();
+  else
+    showClients()
 
-    if(section === 0)
-       showCollection();
-    else
-       showClients()
-
-   setTimeout(addAnimation,2000);
+  setTimeout(addAnimation, 2000);
 }
 
-function showCollection(){
-    document.getElementById("collectionItemLeft").innerHTML=`
-    <img src="./images/${images[flag]}.png" alt="symbol" class="img-fluid" /><br>
+function showCollection() {
+  document.getElementById("collection-item-left").innerHTML = `
+    <img src="./images/${images[flag]}.png" alt="plant image" class="img-fluid" /><br>
     ${labels[flag]}`;
-    document.getElementById("collectionItemMiddle").innerHTML=`
-    <img src="./images/${images[flag+1]}.png" alt="symbol" class="img-fluid" /><br>
-    ${labels[flag+1]}`;
-    document.getElementById("collectionItemRight").innerHTML=`
-    <img src="./images/${images[flag+2]}.png" alt="symbol" class="img-fluid" /><br>
-    ${labels[flag+2]}`;
+  document.getElementById("collection-item-middle").innerHTML = `
+    <img src="./images/${images[flag + 1]}.png" alt="plant image" class="img-fluid" /><br>
+    ${labels[flag + 1]}`;
+  document.getElementById("collection-item-right").innerHTML = `
+    <img src="./images/${images[flag + 2]}.png" alt="plant inage" class="img-fluid" /><br>
+    ${labels[flag + 2]}`;
 }
 
-function showClients(){
-    document.getElementById("clientsItem").innerHTML=`
-    <p class="clientsPara p-3 ps-xl-5 ms-xl-5">
+function showClients() {
+  document.getElementById("clients-item").innerHTML = `
+    <p class="clients-para p-3 ps-xl-5 ms-xl-5">
     <i class="fs-1 fa-solid fa-quote-left fa-2xl text-white"></i><br>
     It is a long established fact that a reader will be distracted by the readable content of a page when It
     is a long . It is a long .It is a long established fact that a reader will be . It is a long
     established fact that a reader .
   <i class="fs-1 fa-solid fa-quote-right fa-2xl text-white"></i>
   </p>
-  <img class="pt-3 clientsItem" src="./images/${clients[flag]}.png" alt="symbol" />`;
+  <img class="pt-3 clients-item" src="./images/${clients[flag]}.png" alt="symbol" />`;
 }
 
 
-function showPlants(){
-    document.getElementById("heroRight").classList.toggle("heroItemRight");
-    document.getElementById("heroRight").classList.toggle("heroCenterItem");
-    document.getElementById("heroCenter").classList.toggle("heroItemCenter");
-    document.getElementById("heroCenter").classList.toggle("heroRightItem");
-    document.getElementById("heroRight").classList.add("collectionItem");
-    document.getElementById("heroCenter").classList.add("collectionItem");
-    document.getElementById("sliderLeft").classList.toggle("headingText");
-    document.getElementById("sliderRight").classList.toggle("headingText");
-    setTimeout(addAnimation,2000);
+function showPlants() {
+  document.getElementById("hero-right").classList.toggle("hero-item-right");
+  document.getElementById("hero-right").classList.toggle("hero-center-item");
+  document.getElementById("hero-center").classList.toggle("hero-item-center");
+  document.getElementById("hero-center").classList.toggle("hero-right-item");
+  document.getElementById("hero-right").classList.add("collection-item");
+  document.getElementById("hero-center").classList.add("collection-item");
+  document.getElementById("slider-left").classList.toggle("heading-text");
+  document.getElementById("slider-right").classList.toggle("heading-text");
+  setTimeout(addAnimation, 2000);
 }
 
-function addAnimation(){
-    document.getElementById("collectionItemLeft").classList.remove("collectionItem");
-    document.getElementById("collectionItemMiddle").classList.remove("collectionItem");
-    document.getElementById("collectionItemRight").classList.remove("collectionItem");
-    document.getElementById("clientsItem").classList.remove("collectionItem");
-    document.getElementById("heroRight").classList.remove("collectionItem");
-    document.getElementById("heroCenter").classList.remove("collectionItem");
-    
+function addAnimation() {
+  document.getElementById("collection-item-left").classList.remove("collection-item");
+  document.getElementById("collection-item-middle").classList.remove("collection-item");
+  document.getElementById("collection-item-right").classList.remove("collection-item");
+  document.getElementById("clients-item").classList.remove("collection-item");
+  document.getElementById("hero-right").classList.remove("collection-item");
+  document.getElementById("hero-center").classList.remove("collection-item");
+
 }
